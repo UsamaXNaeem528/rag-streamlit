bc657c4cd6fcc9ba5bab82fa2d220dcdbe5ce594
Usama Naeem <UsamaXNaeem528>
2026-08-19 23:38:10 +0500

    push mmr

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ template = ChatPromptTemplate.from_messages([
 
 def get_retriever(vector_store):
     retriever = vector_store.as_retriever(
-          search_type="similarity",
+          search_type="mmr",
         search_kwargs={
             "k": 5,
             "fetch_k": 10,
