d3f89768f23e44533d7f4365c8edf5867a5dfb36
Usama Naeem <UsamaXNaeem528>
2026-08-19 23:35:53 +0500

    change search method to mmr

--- main.py
+++ main.py
@@ -52,8 +52,10 @@ def get_retriever(vector_store):
     retriever = vector_store.as_retriever(
           search_type="similarity",
         search_kwargs={
-        "k": 5,
-    }
+            "k": 5,
+            "fetch_k": 10,
+            "lambda_mult": 0.7
+        }
     )
 
     return retriever
